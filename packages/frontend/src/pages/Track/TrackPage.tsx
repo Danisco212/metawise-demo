f31bb3b7998d0aaf89fa62507620363c246940b6
Gustav Albrecht <gustav.albrecht@iths.se>
2022-11-16 22:10:30 +0100

    Fixed bugs in recording function

--- packages/frontend/src/pages/Track/TrackPage.tsx
+++ packages/frontend/src/pages/Track/TrackPage.tsx
@@ -8,6 +8,8 @@ import Pulser from "../../components/Pulser/Pulser";
 import { SideMenu } from "../../components/SideMenu";
 
 let notion:any
+let notion2: any
+let notion3: any
 const TrackPage = (props: any) => {
     const [tracking, setTracking] = useState(false)
     const [trackedData, setTrackedData] = useState<any>([])
@@ -16,26 +18,26 @@ const TrackPage = (props: any) => {
     const randomIntFromInterval = (min: number, max: number) => { // min and max included 
         return Math.floor(Math.random() * (max - min + 1) + min)
     }
-    useEffect(() => {
+   /*  useEffect(() => {
         return () => {
             if(notion){
                 startReading()
             }
         }
-    }, [])
+    }, []) */
     const startReading = () => {
         if(props.authData){
             setTracking(true)
             notion = props.authData.notion.brainwaves("powerByBand").subscribe((brainwaves: any) => {
                 setTrackedData((prev: any) => [...prev, brainwaves])
             });
-            notion = props.authData.notion.brainwaves("raw").subscribe((brainwaves: any) => {
+            notion2 = props.authData.notion.brainwaves("raw").subscribe((brainwaves: any) => {
                 setTrackedDataEEG((prev: any) => [...prev, brainwaves])
-                console.log("eegdata: ", brainwaves)
+                //console.log("eegdata: ", brainwaves)
             });
-            notion = props.authData.notion.signalQuality().subscribe((signalQuality: any) => {
+            notion3 = props.authData.notion.signalQuality().subscribe((signalQuality: any) => {
                 setTrackedDataSignalQuality((prev: any) => [...prev, signalQuality])
-                console.log("signalquality: ", signalQuality)
+                //console.log("signalquality: ", signalQuality)
             });
         }
     }
@@ -46,14 +48,26 @@ const TrackPage = (props: any) => {
         
         setTracking(false)
         notion.unsubscribe();
-        console.log('the tracked data is', trackedDataSignalQuality, trackedDataEEG, trackedData);
+        notion2.unsubscribe();
+        notion3.unsubscribe();
+        
+        console.log('the tracked data is', trackedDataSignalQuality, trackedDataEEG, trackedData); 
+      
         // hard coded saving
-        axios.post("http://localhost:5676/api/v1/powerband/save", { signalquality: trackedDataSignalQuality, raweeg: trackedDataEEG ,powerband: trackedData })
+        axios.post("http://localhost:5676/api/v1/powerband/save", JSON.stringify({
+             signalquality: trackedDataSignalQuality, 
+             raweeg: trackedDataEEG,
+             powerband: trackedData }),  { headers: {
+                // Overwrite Axios's automatically set Content-Type
+                'Content-Type': 'application/json'
+            }})
         .then(res => toast("Data saved successfully"))
         .catch(err => toast.error("Data failed to save"))
-        setTrackedData([]);
+      
+         setTrackedData([]);
         setTrackedDataEEG([]);
-        setTrackedDataSignalQuality([]);
+        setTrackedDataSignalQuality([])
+        
     }
     return(
         <div className="h-screen bg-slate-200">
